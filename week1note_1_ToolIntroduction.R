355b50a66e6e52ead67a56d69fbad992331eb5ce
haorenxwx <haorenxwx@users.noreply.github.com>
2017-10-26 22:55:28 +0800

    20171026
    changePC

---
+++ week1note_1_ToolIntroduction.R
@@ -0,0 +1,17 @@
+important R function
+ ?rnorm
+ #access help files
+
+ help.search("rnorm")
+ #search help files
+
+ >args("rnorm")
+ #get arguments
+
+ >function(n,mean = 0,sd=1)
+ >NULL
+
+ >rnom
+ #show the entire code
+
+
