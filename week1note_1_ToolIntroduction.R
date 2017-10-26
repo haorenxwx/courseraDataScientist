a02628bafcfc75befcfc11a7b0d07b98e66d9b9b
haorenxwx <haorenxwx@users.noreply.github.com>
2017-10-26 23:18:18 +0800

    20171026
    o-0

--- week1note_1_ToolIntroduction.R
+++ week1note_1_ToolIntroduction.R
@@ -5,13 +5,37 @@ important R function
  help.search("rnorm")
  #search help files
 
- >args("rnorm")
+ args("rnorm")
  #get arguments
 
- >function(n,mean = 0,sd=1)
- >NULL
-
- >rnom
+ function(n,mean = 0,sd=1)
+ NULL
+ rnom
  #show the entire code
+
+example of R function:
+1:Reading lines of a textfile(on webpage)
+con <- url("http://www.jhsph.edu","r")
+x <- readlines(con)
+> head(x)
+#会显示从网页读出来的html
+
+2:lapply:
+takes 3 arguments(list x,function(or function_name),other arguments)
+The actual looping is done internally in C code.
+
+content of data 
+1:connecting data base
+ucsDb <- dbConnect(MySQL(), user = "genome", host = "genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucsDb, "show databases;")
+dbDisconnect(ucsDb)
+result
+
+2: merging data-merge()
+mergedData2 <- merge(reviews, solutions, by.x = "solution_id", by.y = "id",
+		all = TRUE)
+head(mergedData2[, 1:6],3)
+reviews[1,1:6]
+
 
 
